a07e3240eec2501b16f6ac657d38bd9ee57baf4e
3bgawad <73382682+3bgawad@users.noreply.github.com>
2022-02-04 15:59:08 +0200

    Scheduler update

--- scheduler.c
+++ scheduler.c
@@ -6,6 +6,18 @@
 struct PCB * pcbPointer=NULL;
 enum Status{Free, Busy};
 
+void Merge(int address, int size);
+void DeAllocate_memory(int address, int size);
+int Allocate_memory(int size);
+
+//memory management arrays creation 
+//Initially the memory is divided into 4 partitions each have 256 bytes
+//The values stored in the array refears to block's starting address in memory
+int blocks_32[32];
+int blocks_64[16];
+int blocks_128[8];
+int blocks_256[4] = {0, 256, 512, 768};
+
 struct msgbuff
 {
     long mtype;
@@ -51,7 +63,19 @@ int main(int argc, char * argv[])
     //defining the output files for write
     FILE *logFile = fopen("scheduler.log", "w");
     FILE *prefFile = fopen("scheduler.pref", "w");
+    FILE * sizeP = fopen("memory.log", "w");
     
+    fprintf(logFile, "\n#At time x process y state arr w total z remain y wait k\n");
+
+    //Initialize for memory blocks all other arrays with -1
+    for (int i = 0; i < 32; i++)
+        blocks_32[i]=-1;
+    for (int i = 0; i < 16; i++)
+        blocks_64[i]=-1;
+    for (int i = 0; i < 8; i++)
+        blocks_128[i]=-1;
+
+    fprintf(sizeP, "\n#At time x allocated y bytes for process z from i to j\n");
     fprintf(logFile, "\n#At time x process y state arr w total z remain y wait k\n");
     
 
@@ -64,6 +88,9 @@ int main(int argc, char * argv[])
     //RR
     Queue RR_readyQueue;
     queueInit(&RR_readyQueue, sizeof(struct processData));
+    //SJF
+    PQueue SJF_readyQueue;
+    PQueueInit(&SJF_readyQueue);
 
     //scan the count of the process
     sscanf(argv[2],"%d",&pcount);
@@ -99,6 +126,7 @@ int main(int argc, char * argv[])
                 pData = message.mData;
                 printf("\nTime = %d process with id = %d recieved\n", getClk(), pData.id);
                 push(&HPF_readyQueue, pData.priority, pData); //enqueue the data in the ready queue
+                
                 //Creating PCB
                 pcbBlock.state = 0;
                 pcbBlock.executionTime = 0;
@@ -136,7 +164,7 @@ int main(int argc, char * argv[])
             }
             sleep(1000);
             //Update pcb and calculate the process data after finishing it
-            pcbPointer->executionTime=pData.runningtime;
+            pcbPointer->executionTime=pData.runningtime;    
             pcbPointer->waitingTime=getClk()-(pData.arrivaltime)-(pcbPointer->executionTime);
             pcbPointer->state=Finished;
             pcbPointer->remainingTime=0;
@@ -318,16 +346,23 @@ int main(int argc, char * argv[])
         enqueue(&RR_readyQueue, &pData); //enqueue the data in the ready queue
         printf("\n%d %d %d %d\n",pData.id,pData.arrivaltime,pData.runningtime,pData.priority);
         if(receive!=-1)
-        {
-            //Creating PCB
-            pcbBlock.state = 0;
-            pcbBlock.executionTime = 0;
-            pcbBlock.remainingTime = pData.runningtime;
-            pcbBlock.waitingTime = 0;
-            
-            insertFirst(pData.id, pcbBlock);
-            printf("\nPCB created for process with Pid = %d\n",pData.id);
-        }
+            {
+                //Allocate Memory
+                //Get the power of 2 value of size
+                int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+                int address = Allocate_memory(size);
+                fprintf(sizeP, "\nAt time %d allocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, address,address+size-1);
+
+
+                //Creating PCB
+                pcbBlock.state = 0;
+                pcbBlock.executionTime = 0;
+                pcbBlock.remainingTime = pData.runningtime;
+                pcbBlock.waitingTime = 0;
+                pcbBlock.mem_address=address;
+                insertFirst(pData.id, pcbBlock);
+                printf("\nPCB created\n");
+            }
         
         while(getQueueSize(&RR_readyQueue)!=0 || pcount!=0)
         {   
@@ -340,13 +375,19 @@ int main(int argc, char * argv[])
                 pData = message.mData;
                 enqueue(&RR_readyQueue, &pData);
                 printf("\n%d %d %d %d\n",pData.id,pData.arrivaltime,pData.runningtime,pData.priority);
+                
+                //Allocate Memory
+                //Get the power of 2 value of size
+                int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+                int address = Allocate_memory(size);
+                fprintf(sizeP, "\nAt time %d allocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, address,address+size-1);
 
                  //Creating PCB
                 pcbBlock.state = 0;
                 pcbBlock.executionTime = 0;
                 pcbBlock.remainingTime = pData.runningtime;
                 pcbBlock.waitingTime = 0;
-
+                pcbBlock.mem_address=address;
                 insertFirst(pData.id, pcbBlock);
                 printf("\nPCB created\n");
                 receive = msgrcv(msgqid, &message, sizeof(message.mData), 0, IPC_NOWAIT);
@@ -401,6 +442,13 @@ int main(int argc, char * argv[])
                     WTAArray[pData.id] = WTA;
                     sum_Waiting+=pcbPointer->waitingTime;
                     sum_RunningTime+=pcbPointer->executionTime;
+
+                   //Deallocate memory for the finished process
+                    int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+                    DeAllocate_memory(pcbPointer->mem_address,size);
+                    fprintf(sizeP, "\nAt time %d deallocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, pcbPointer->mem_address, pcbPointer->mem_address+size-1);
+                    pData.id=-1;
+
                 }
                 
             }
@@ -464,6 +512,13 @@ int main(int argc, char * argv[])
                         sum_Waiting+=pcbPointer->waitingTime;
                         sum_RunningTime+=pcbPointer->executionTime;
 
+                        //Deallocate memory for the finished process
+                        int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+                        DeAllocate_memory(pcbPointer->mem_address,size);
+                        fprintf(sizeP, "\nAt time %d deallocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, pcbPointer->mem_address, pcbPointer->mem_address+size-1);
+                        pData.id=-1;
+
+
                     }
                     
                 }
@@ -481,6 +536,12 @@ int main(int argc, char * argv[])
                     sum_Waiting+=pcbPointer->waitingTime;
                     sum_RunningTime+=pcbPointer->executionTime;
 
+                   //Deallocate memory for the finished process
+                    int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+                    DeAllocate_memory(pcbPointer->mem_address,size);
+                    fprintf(sizeP, "\nAt time %d deallocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, pcbPointer->mem_address, pcbPointer->mem_address+size-1);
+                    pData.id=-1;
+
                 }
             }
            
@@ -489,7 +550,104 @@ int main(int argc, char * argv[])
        
         break;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        //SJF
+        case 4:
+        receive= msgrcv(msgqid, &message, sizeof(message.mData), 0, !IPC_NOWAIT);
+        pData = message.mData;
+        printf("\nTime = %d process with id = %d recieved\n", getClk(), pData.id);
+        
+
+        //enqueue the process in the ready queue
+        push(&SJF_readyQueue, pData.runningtime, pData);
+       
+        //Allocate Memory
+        //Get the power of 2 value of size
+        int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+        int address = Allocate_memory(size);
+        fprintf(sizeP, "\nAt time %d allocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, address,address+size);
+
+        //Create PCB
+        pcbBlock.state = 0;
+        pcbBlock.executionTime = 0;
+        pcbBlock.remainingTime = pData.runningtime;
+        pcbBlock.waitingTime = 0;
+        pcbBlock.mem_address = address;
+        insertFirst(pData.id, pcbBlock);
+        printf("\nPCB created for process with id = %d\n", pData.id);
+
+        while (pcount!=0)
+        {
+            printf("\nThe current time = %d\n", getClk());
+            receive = msgrcv(msgqid, &message, sizeof(message.mData), 0, IPC_NOWAIT);
+            while (receive != -1)
+            {
+                pData = message.mData;
+                printf("\nTime = %d process with id = %d recieved\n", getClk(), pData.id);
+                push(&SJF_readyQueue, pData.runningtime, pData); //enqueue the data in the ready queue
+                //Allocate Memory
+                //Get the power of 2 value of size
+                int size = pow(2,ceil(log(pData.sizeP)/log(2)));
+                int address = Allocate_memory(size);
+                fprintf(sizeP, "\nAt time %d allocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, address,address+size);
+
+                //Creating PCB
+                pcbBlock.state = 0;
+                pcbBlock.executionTime = 0;
+                pcbBlock.remainingTime = pData.runningtime;
+                pcbBlock.waitingTime = 0;
+                pcbBlock.mem_address = address;
+            
+                insertFirst(pData.id, pcbBlock);
+                printf("\nPCB created for process with id = %d\n", pData.id);
+                receive = msgrcv(msgqid, &message, sizeof(message.mData), 0, IPC_NOWAIT);
+                
+            }
+            pData=pop(&SJF_readyQueue);
+            //find the pcb of the poped process
+            pcbFind = find(pData.id);
+            pcbPointer = &(pcbFind->data);
+            pcbPointer->waitingTime=getClk()-(pData.arrivaltime)-(pcbPointer->executionTime);
+            //write in the Log file the process data
+            fprintf(logFile, "At time %d process %d started arr %d total %d remain %d wait %d\n", getClk(), pData.id, pData.arrivaltime, pData.runningtime, pcbPointer->remainingTime, pcbPointer->waitingTime);
+
+            //fork the process
+            printf("\nIam forking a new process time = %d\n", getClk());
+            int pid=fork();
+            pcount--;
+            pcbPointer->pid=pid;
+            if (pid == -1)
+            perror("error in forking");
+
+            else if (pid == 0)
+            {
+                printf("\ntest the forking\n");
+                char buf[20];
+                sprintf(buf,"%d",pData.runningtime);
+                char *argv[] = { "./process.out",buf, 0 };
+                execve(argv[0], &argv[0], NULL);
+            }
+            sleep(50);
+            //Update pcb and calculate the process data after finishing it
+            pcbPointer->executionTime=pData.runningtime;
+            pcbPointer->waitingTime=getClk()-(pData.arrivaltime)-(pcbPointer->executionTime);
+            pcbPointer->state=Finished;
+            pcbPointer->remainingTime=0;
+            int TA = getClk()-(pData.arrivaltime);
+            double WTA=(double)TA/pData.runningtime;
+            fprintf(logFile, "At time %d process %d finished arr %d total %d remain %d wait %d TA %d WTA %.2f\n", getClk(), pData.id, pData.arrivaltime, pData.runningtime, pcbPointer->remainingTime, pcbPointer->waitingTime, TA, WTA);
+            sum_WTA+=WTA;
+            WTAArray[pData.id] = WTA;
+            sum_Waiting+=pcbPointer->waitingTime;
+            sum_RunningTime+=pcbPointer->executionTime;
+            //Deallocate the memory 
+            size = pow(2,ceil(log(pData.sizeP)/log(2)));
+            DeAllocate_memory(pcbPointer->mem_address, size);
+            fprintf(sizeP, "\nAt time %d deallocated %d bytes for process %d from %d to %d\n", getClk(), pData.sizeP, pData.id, pcbPointer->mem_address,pcbPointer->mem_address+size);
+
+        }
+        break;
 
     }
     fclose(logFile);
@@ -507,6 +665,9 @@ int main(int argc, char * argv[])
     //upon termination release the clock resources
     
     destroyClk(false);
+
+    
+
 }
 
 
@@ -526,4 +687,275 @@ void Handler(int signum)
             pcbPointer->remainingTime=0;
         }
     }
+}
+
+int Allocate_memory(int size)
+{
+    switch (size)
+    {
+    case 32:
+        for (int i = 0; i < 32; i++)
+        {
+            if(blocks_32[i]!=-1)
+            {
+                blocks_32[i]=-1;
+                return i*size;
+            }
+
+        }
+
+
+        for (int i = 0; i < 16; i++)
+        {
+            if(blocks_64[i]!=-1)
+            {
+                blocks_32[2*i+1]=(2*i+1)*size;
+                blocks_64[i]=-1;
+                return (2*i)*size;
+            }
+
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            if(blocks_128[i]!=-1)
+            {
+                blocks_64[2*i+1]=(2*i+1)*2*size;
+                blocks_32[4*i+1]=(4*i+1)*size;
+                blocks_128[i]=-1;
+                return (4*i)*size;
+            }
+
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if(blocks_256[i]!=-1)
+            {
+                blocks_128[2*i+1]=(2*i+1)*4*size;
+                blocks_64[4*i+1]=(4*i+1)*2*size;
+                blocks_32[8*i+1]=(8*i+1)*size;
+                blocks_256[i]=-1;
+                return (8*i)*size;
+            }
+        }
+
+        printf("\n\n Warning! There's no enough space in memory for this process\n");
+        
+        break;
+
+    case 64:
+        for (int i = 0; i < 16; i++)
+        {
+            if(blocks_64[i]!=-1)
+            {
+
+                blocks_64[i]=-1;
+                return (i)*size;
+            }
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            if(blocks_128[i]!=-1)
+            {
+
+                blocks_64[2*i+1]=(2*i+1)*size;
+                blocks_128[i]=-1;
+                return (2*i)*size;
+            }
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if(blocks_256[i]!=-1)
+            {
+
+                blocks_128[2*i+1]=(2*i+1)*2*size;
+                blocks_64[4*i+1]=(4*i+1)*size;
+                blocks_256[i]=-1;
+                return (4*i)*size;
+            }
+        }
+        printf("\n\n Warning! There's no enough space in memory for this process\n");
+        break;
+
+    case 128:
+        for (int i = 0; i < 8; i++)
+        {
+
+            if(blocks_128[i]!=-1)
+            {
+                blocks_128[i]=-1;
+                return (i)*size;
+            }
+        }
+        for (int i = 0; i < 4; i++)
+        {
+
+            if(blocks_256[i]!=-1)
+            {
+                blocks_128[2*i+1]=(2*i+1)*size;
+                blocks_256[i]=-1;
+                return (2*i)*size;
+            }
+        }
+        printf("\n\n Warning! There's no enough space in memory for this process\n");
+        break;
+
+    case 256:
+        for (int i = 0; i < 4; i++)
+        {
+
+            if(blocks_256[i]!=-1)
+            {
+                blocks_256[i]=-1;
+                return (i)*size;
+            }
+        }
+        printf("\n\n Warning! There's no enough space in memory for this process\n");
+        break;
+
+    }
+}
+
+void DeAllocate_memory(int address, int size)
+{
+    int ratio=address/size;
+    switch (size)
+    {
+    case 32:
+
+
+        blocks_32[ratio]=address;
+
+        break;
+    
+    case 64:
+        blocks_64[ratio]=address;
+
+        break;
+    
+    case 128:
+
+        blocks_128[ratio]=address;
+
+        break;
+
+    case 256:
+
+        blocks_256[ratio]=address;
+
+        break;
+    }
+
+    printf("\n [256] Memory Blocks: \n");
+    for (int i = 0; i < 4 ; i++) printf("%d\t", blocks_256[i]);
+
+    printf("\n[128] Memory Blocks:\n");
+    for (int i = 0; i < 8; i++) printf("%d\t", blocks_128[i]);
+
+    printf("\n[64] Memory Blocks:\n");
+    for (int i = 0; i < 16; i++) printf("%d\t", blocks_64[i]);
+
+    printf("\n[32] Memory Blocks:\n");
+    for (int i = 0; i < 32; i++) printf("%d\t", blocks_32[i]);
+
+    printf("\n\n");
+
+
+    Merge(address, size);
+
+
+    printf("\n [256] Memory Blocks: \n");
+    for (int i = 0; i < 4 ; i++) printf("%d\t", blocks_256[i]);
+
+    printf("\n[128] Memory Blocks:\n");
+    for (int i = 0; i < 8; i++) printf("%d\t", blocks_128[i]);
+
+    printf("\n[64] Memory Blocks:\n");
+    for (int i = 0; i < 16; i++) printf("%d\t", blocks_64[i]);
+
+    printf("\n[32] Memory Blocks:\n");
+    for (int i = 0; i < 32; i++) printf("%d\t", blocks_32[i]);
+
+    printf("\n\n");
+}
+
+void Merge(int address, int size)
+{
+    //Implementation of merging memory blocks goes here
+    switch (size)
+    {
+    case 32:
+        for (int i = 0; i< 32; i+=2)
+        {
+          if (blocks_32[i] != -1 && blocks_32[i+1] != -1)
+          {
+           blocks_64[i/2]= i*size;
+           blocks_32[i] = -1;
+           blocks_32[i+1]=-1;
+           printf("\nmemory 32 merged successfully\n");
+          }
+        } 
+
+        for (int i = 0; i < 16; i+=2)
+        {
+            if (blocks_64[i] != -1 && blocks_64[i+1] != -1)
+            {
+                blocks_128[i/2] = 2*i*size;
+                blocks_64[i] = -1;
+                blocks_64[i+1] = -1;
+                printf("\nmemory 64 merged successfully\n");
+            }
+        }
+
+        for (int i = 0; i < 8; i +=2)
+        {
+            if (blocks_128[i] != -1 && blocks_128[i+1] != -1)
+            {
+                blocks_256[i/2] = 4*i*size;
+                blocks_128[i] = -1;
+                blocks_128[i+1] = -1;
+                printf("\nmemory 128 merged successfully\n");
+            }
+        }
+        break;
+    
+    case 64:
+        for (int i = 0; i < 16; i+= 2)
+        {
+            if (blocks_64[i] != -1 && blocks_64[i+1] != -1)
+            {
+                blocks_128[i/2] = i*size;
+                blocks_64[i] = -1;
+                blocks_64[i+1] = -1;
+                printf("\nmemory 64 merged successfully\n");
+            }
+        }
+
+          for (int i = 0; i < 8; i +=2)
+        {
+            if (blocks_128[i] != -1 && blocks_128[i+1] != -1)
+            {
+                blocks_256[i/2] = 2*i*size;
+                blocks_128[i] = -1;
+                blocks_128[i+1] = -1;
+                printf("\nmemory 128 merged successfully\n");
+            }
+        }
+
+        break;
+
+    case 128:
+        for (int i = 0; i < 8; i +=2)
+        {
+            if (blocks_128[i] != -1 && blocks_128[i+1] != -1)
+            {
+                blocks_256[i/2] = i*size;
+                blocks_128[i] = -1;
+                blocks_128[i+1] = -1;
+                printf("\nmemory 128 merged successfully\n");
+            }
+        }
+        break;
+    }
+
 }
